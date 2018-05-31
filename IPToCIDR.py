7fdc5359fb6d65d746279bc2684a6ea5d01a1ab8
ellinx <ellin.xll@gmail.com>
2018-05-30 19:25:44 -0700

    Add IP to CIDR.

---
+++ IPToCIDR.py
@@ -0,0 +1,78 @@
+"""
+Given a start IP address ip and a number of ips we need to cover n, return a representation of the range as a list (of smallest possible length) of CIDR blocks.
+
+A CIDR block is a string consisting of an IP, followed by a slash, and then the prefix length.
+For example: "123.45.67.89/20". That prefix length "20" represents the number of common prefix bits in the specified range.
+
+Example 1:
+Input: ip = "255.0.0.7", n = 10
+Output: ["255.0.0.7/32","255.0.0.8/29","255.0.0.16/32"]
+Explanation:
+The initial ip address, when converted to binary, looks like this (spaces added for clarity):
+255.0.0.7 -> 11111111 00000000 00000000 00000111
+The address "255.0.0.7/32" specifies all addresses with a common prefix of 32 bits to the given address,
+ie. just this one address.
+
+The address "255.0.0.8/29" specifies all addresses with a common prefix of 29 bits to the given address:
+255.0.0.8 -> 11111111 00000000 00000000 00001000
+Addresses with common prefix of 29 bits are:
+11111111 00000000 00000000 00001000
+11111111 00000000 00000000 00001001
+11111111 00000000 00000000 00001010
+11111111 00000000 00000000 00001011
+11111111 00000000 00000000 00001100
+11111111 00000000 00000000 00001101
+11111111 00000000 00000000 00001110
+11111111 00000000 00000000 00001111
+
+The address "255.0.0.16/32" specifies all addresses with a common prefix of 32 bits to the given address,
+ie. just 11111111 00000000 00000000 00010000.
+
+In total, the answer specifies the range of 10 ips starting with the address 255.0.0.7 .
+
+There were other representations, such as:
+["255.0.0.7/32","255.0.0.8/30", "255.0.0.12/30", "255.0.0.16/32"],
+but our answer was the shortest possible.
+
+Also note that a representation beginning with say, "255.0.0.7/30" would be incorrect,
+because it includes addresses like 255.0.0.4 = 11111111 00000000 00000000 00000100
+that are outside the specified range.
+
+Note:
+ip will be a valid IPv4 address.
+Every implied address ip + x (for x < n) will be a valid IPv4 address.
+n will be an integer in the range [1, 1000].
+"""
+class Solution:
+    def ipToCIDR(self, ip, n):
+        """
+        :type ip: str
+        :type n: int
+        :rtype: List[str]
+        """
+        # start to combine IPs from IP
+        # lowest set bit of a number:
+        def ipNumToStr(num):
+            stk = collections.deque()
+            ret = ''
+            while num:
+                stk.append(num%256)
+                num //=256
+            for i in range(4-len(stk)):
+                stk.append(0)
+            for each in reversed(stk):
+                ret += str(each)+'.'
+            return ret[:-1]
+
+        ret = []
+        ipNum = 0
+        for each in ip.split('.'):
+            ipNum = ipNum*256 + int(each)
+        while n>0:
+            combineRange = ipNum&(-ipNum)
+            while n<combineRange:
+                combineRange >>= 1
+            ret.append(ipNumToStr(ipNum)+'/'+str(32-int(math.log2(combineRange))))
+            n -= combineRange
+            ipNum += combineRange
+        return ret
