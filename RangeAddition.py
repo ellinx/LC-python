6b9eec0de52e81b1a7447737aca5495c362e9d26
ellinx <ellin.xll@gmail.com>
2018-06-07 20:07:43 -0700

    Add Range Addition.

---
+++ RangeAddition.py
@@ -0,0 +1,55 @@
+"""
+Assume you have an array of length n initialized with all 0's and are given k update operations.
+
+Each operation is represented as a triplet: [startIndex, endIndex, inc] which increments each element of subarray A[startIndex ... endIndex] (startIndex and endIndex inclusive) with inc.
+
+Return the modified array after all k operations were executed.
+
+Example:
+
+Given:
+
+    length = 5,
+    updates = [
+        [1,  3,  2],
+        [2,  4,  3],
+        [0,  2, -2]
+    ]
+
+Output:
+
+    [-2, 0, 3, 5, 3]
+
+Explanation:
+
+Initial state:
+[ 0, 0, 0, 0, 0 ]
+
+After applying operation [1, 3, 2]:
+[ 0, 2, 2, 2, 0 ]
+
+After applying operation [2, 4, 3]:
+[ 0, 2, 5, 5, 3 ]
+
+After applying operation [0, 2, -2]:
+[-2, 0, 3, 5, 3 ]
+
+"""
+class Solution:
+    def getModifiedArray(self, length, updates):
+        """
+        :type length: int
+        :type updates: List[List[int]]
+        :rtype: List[int]
+        """
+        # process each operation by adding inc at startIndex and subtracting inc at endIndex+1
+        # final list is list[i] = list[0]+...+list[i-1]
+        ret = [0]*length
+        for update in updates:
+            ret[update[0]] += update[2]
+            if update[1]+1<length:
+                ret[update[1]+1] -= update[2]
+
+        for i in range(1,length):
+            ret[i] += ret[i-1]
+        return ret
