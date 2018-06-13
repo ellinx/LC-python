ed8f38ae972c05fe09555692f57c6335f456f6b0
ellinx <ellin.xll@gmail.com>
2018-06-12 20:42:43 -0700

    Add Valid Parentheses.

---
+++ ValidParentheses.py
@@ -0,0 +1,58 @@
+"""
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+
+Note that an empty string is also considered valid.
+
+Example 1:
+Input: "()"
+Output: true
+
+Example 2:
+Input: "()[]{}"
+Output: true
+
+Example 3:
+Input: "(]"
+Output: false
+
+Example 4:
+Input: "([)]"
+Output: false
+
+Example 5:
+Input: "{[]}"
+Output: true
+
+
+"""
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stk = collections.deque()
+        for c in s:
+            if c=="(" or c=="[" or c=="{":
+                stk.append(c)
+            elif c==")":
+                if not len(stk):
+                    return False
+                if stk.pop()!="(":
+                    return False
+            elif c=="]":
+                if not len(stk):
+                    return False
+                if stk.pop()!="[":
+                    return False
+            elif c=="}":
+                if not len(stk):
+                    return False
+                if stk.pop()!="{":
+                    return False
+        return len(stk)==0
