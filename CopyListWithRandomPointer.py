5b01aa67e64f6d7f56377733f4a2833f73304bc9
ellinx <ellin.xll@gmail.com>
2018-06-22 21:16:11 -0700

    Update Copy List with Random Pointer.

--- CopyListWithRandomPointer.py
+++ CopyListWithRandomPointer.py
@@ -36,3 +36,40 @@ class CopyListWithRandomPointer(object):
                 copyCur.random = copy[cur.random.label]
             copyCur, cur = copyCur.next, cur.next
         return dummy2.next
+
+    # without map
+    def copyRandomList2(self, head):
+        """
+        :type head: RandomListNode
+        :rtype: RandomListNode
+        """
+        # copy original node and combine origianl and copied node in one list
+        # then seperate them
+        if not head:
+            return None
+        # copy nodes
+        cur = head
+        while cur:
+            copied = RandomListNode(cur.label)
+            copied.next = cur.next
+            cur.next = copied
+            cur = copied.next
+        # set random pointer
+        cur = head
+        copied = cur.next
+        while cur:
+            if cur.random:
+                copied.random = cur.random.next
+            cur = copied.next
+            if cur:
+                copied = cur.next
+        # seperate original with copied
+        dummy = RandomListNode(0)
+        cur = head
+        copied = dummy
+        while cur:
+            copied.next = cur.next
+            copied = copied.next
+            cur.next = cur.next.next
+            cur = cur.next
+        return dummy.next
