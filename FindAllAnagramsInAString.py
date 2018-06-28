bfda4986d3ae37e2f7ba48e628eec463557d610c
ellinx <ellin.xll@gmail.com>
2018-06-27 20:07:41 -0700

    Update Find All Anagrams in a String.

--- FindAllAnagramsInAString.py
+++ FindAllAnagramsInAString.py
@@ -32,37 +32,40 @@ The substring with start index = 2 is "ab", which is an anagram of "ab".
 """
 
 
-class FindAllAnagramsInString:
+class Solution:
+    """
+    Thoughts:
+    1. similar to sliding window
+
+    Time: O(m+n) where m is length of s and n is length of p
+    Space: O(n)
+    """
     def findAnagrams(self, s, p):
         """
         :type s: str
         :type p: str
         :rtype: List[int]
         """
-        ret = []
-        if len(s)<len(p):
-            return ret
-        count = collections.Counter(p)
+        counter = collections.Counter(p)
         total = len(p)
+        ret = []
         start, end = 0, 0
         while end<len(s):
-            while end<len(s) and end-start<len(p):
-                if s[end] in count:
-                    count[s[end]] -= 1
-                    if count[s[end]]>=0:
-                        total -= 1
+            if s[end] in counter:
+                if counter[s[end]]>0:
+                    total -= 1
+                counter[s[end]] -=1
+            if end-start+1<len(p):
                 end += 1
-            #print(start,end,total,count)
-            if end-start==len(p):
-                if total==0:
-                    ret.append(start)
-                if s[start] in count:
-                    count[s[start]] += 1
-                    if count[s[start]]>0:
-                        total += 1
-                start += 1
-            else:
-                break
+                continue
+            if total==0:
+                ret.append(start)
+            if s[start] in counter:
+                if counter[s[start]]>=0:
+                    total += 1
+                counter[s[start]] +=1
+            start += 1
+            end += 1
         return ret
 
 
