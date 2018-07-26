83cd9570913aa575b4263530723f02cf0c8ce464
ellinx <ellin.xll@gmail.com>
2018-07-25 20:11:08 -0700

    Update Reverse Words in a String.

--- ReverseWordsInString.py
+++ ReverseWordsInString.py
@@ -11,26 +11,18 @@ A word is defined as a sequence of non-space characters.
 Input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces.
 You need to reduce multiple spaces between two words to a single space in the reversed string.
 """
-class ReverseWordsInString(object):
+class Solution(object):
     def reverseWords(self, s):
         """
         :type s: str
         :rtype: str
         """
-        start, end = 0, 0
-        stk = []
-        s = s.strip()
-        while end<len(s):
-            if s[end]!=' ':
-                end += 1
-                continue
-            stk.append(s[start:end])
-            while end<len(s) and s[end]==' ':
-                end += 1
-            start = end
-        if start<len(s):
-            stk.append(s[start:end])
-        ret = ''
-        for each in reversed(stk):
-            ret += each+' '
-        return ret[:-1]
+        words = s.split(" ")
+        ret = ""
+        for word in reversed(words):
+            word = word.strip()
+            if len(word):
+                ret += " "+word
+        if not len(ret):
+            return ret
+        return ret[1:]
