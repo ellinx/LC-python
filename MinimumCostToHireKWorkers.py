b3c0c49f438cf7cafe11ef0a8a770ae0209b8a5e
ellinx <ellin.xll@gmail.com>
2018-07-27 21:28:17 -0700

    Add Minimum Cost to Hire K Workers.

---
+++ MinimumCostToHireKWorkers.py
@@ -0,0 +1,58 @@
+"""
+There are N workers.  The i-th worker has a quality[i] and a minimum wage expectation wage[i].
+
+Now we want to hire exactly K workers to form a paid group.
+When hiring a group of K workers, we must pay them according to the following rules:
+
+1. Every worker in the paid group should be paid in the ratio of their quality compared to other workers in the paid group.
+2. Every worker in the paid group must be paid at least their minimum wage expectation.
+
+Return the least amount of money needed to form a paid group satisfying the above conditions.
+
+
+
+Example 1:
+
+Input: quality = [10,20,5], wage = [70,50,30], K = 2
+Output: 105.00000
+Explanation: We pay 70 to 0-th worker and 35 to 2-th worker.
+
+Example 2:
+
+Input: quality = [3,1,10,10,1], wage = [4,8,2,2,7], K = 3
+Output: 30.66667
+Explanation: We pay 4 to 0-th worker, 13.33333 to 2-th and 3-th workers seperately.
+
+
+
+Note:
+
+1. 1 <= K <= N <= 10000, where N = quality.length = wage.length
+2. 1 <= quality[i] <= 10000
+3. 1 <= wage[i] <= 10000
+4. Answers within 10^-5 of the correct answer will be considered correct.
+
+
+"""
+class Solution:
+    def mincostToHireWorkers(self, quality, wage, K):
+        """
+        :type quality: List[int]
+        :type wage: List[int]
+        :type K: int
+        :rtype: float
+        """
+        wpq = [[wage[i]/quality[i],i] for i in range(len(quality))]
+        wpq.sort()
+        #print(wpq)
+        maxHeap = []
+        total = 0
+        ret = float("inf")
+        for i in range(len(wpq)):
+            total += quality[wpq[i][1]]
+            heapq.heappush(maxHeap, -quality[wpq[i][1]])
+            if len(maxHeap)>K:
+                total += heapq.heappop(maxHeap)
+            if len(maxHeap)==K:
+                ret = min(ret, total*wpq[i][0])
+        return ret
