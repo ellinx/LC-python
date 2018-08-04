0a8279ba660ef090d99f0931e99cbdc4749d3f7f
ellinx <ellin.xll@gmail.com>
2018-08-03 21:06:10 -0700

    Update Recover Binary Search Tree.

--- RecoverBinarySearchTree.py
+++ RecoverBinarySearchTree.py
@@ -52,53 +52,21 @@ class Solution:
         :type root: TreeNode
         :rtype: void Do not return anything, modify root in-place instead.
         """
-        def dfs(cur, swap):
-            if not cur:
+        def dfs(root, swap):
+            if root is None:
                 return
-            dfs(cur.left, swap)
+            dfs(root.left, swap)
             nonlocal pre
-            if not swap[0]:
-                # find swap1
-                if pre and pre.val>cur.val:
+            if pre and pre.val>root.val:
+                if swap[0] is None:
                     swap[0] = pre
-                    swap[1] = cur
-            else:
-                # find largest value that's smaller than swap[0].val and store in swap[1]
-                if cur.val<swap[0].val:
-                    swap[1] = cur
-            pre = cur
-            dfs(cur.right, swap)
+                    swap[1] = root
+                else:
+                    swap[1] = root
+            pre = root
+            dfs(root.right, swap)
 
         swap = [None]*2
         pre = None
         dfs(root, swap)
         swap[0].val, swap[1].val = swap[1].val, swap[0].val
-
-    # O(n) space
-    def recoverTree2(self, root):
-        """
-        :type root: TreeNode
-        :rtype: void Do not return anything, modify root in-place instead.
-        """
-        def dfs(root, li):
-            if not root:
-                return
-            dfs(root.left, li)
-            li.append(root)
-            dfs(root.right, li)
-
-        li = []
-        dfs(root, li)
-        # find the none ascending pair
-        for i in range(len(li)-1):
-            if li[i].val>li[i+1].val:
-                break
-        # find the largest value that's smaller than the li[i].val after index i and swap them
-        start, end = i+1, len(li)-1
-        while start<=end:
-            mid = start+(end-start)//2
-            if li[mid].val>li[i].val:
-                end = mid-1
-            else:
-                start = mid+1
-        li[start-1].val, li[i].val = li[i].val, li[start-1].val
