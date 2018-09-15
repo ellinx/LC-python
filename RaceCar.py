85ba89316a58dfd68c10ba91c185f5c30b019664
ellinx <ellin.xll@gmail.com>
2018-09-14 20:22:45 -0700

    Add Race Car.

---
+++ RaceCar.py
@@ -0,0 +1,66 @@
+"""
+Your car starts at position 0 and speed +1 on an infinite number line.  (Your car can go into negative positions.)
+
+Your car drives automatically according to a sequence of instructions A (accelerate) and R (reverse).
+
+When you get an instruction "A", your car does the following: position += speed, speed *= 2.
+
+When you get an instruction "R", your car does the following:
+if your speed is positive then speed = -1 , otherwise speed = 1.  (Your position stays the same.)
+
+For example, after commands "AAR", your car goes to positions 0->1->3->3, and your speed goes to 1->2->4->-1.
+
+Now for some target position, say the length of the shortest sequence of instructions to get there.
+
+Example 1:
+Input:
+target = 3
+Output: 2
+Explanation:
+The shortest instruction sequence is "AA".
+Your position goes from 0->1->3.
+
+Example 2:
+Input:
+target = 6
+Output: 5
+Explanation:
+The shortest instruction sequence is "AAARA".
+Your position goes from 0->1->3->7->7->6.
+
+
+Note:
+1 <= target <= 10000.
+"""
+class Solution:
+    def racecar(self, target):
+        """
+        :type target: int
+        :rtype: int
+        """
+        if target==0:
+            return 0
+        visited = collections.defaultdict(set)
+        visited[0].add(1)
+        q = collections.deque()
+        q.append((0,1))
+        step = 0
+        while len(q):
+            #print(step,q)
+            size = len(q)
+            for _ in range(size):
+                cur = q.popleft()
+                # A
+                nxt = (cur[0]+cur[1], cur[1]*2)
+                if nxt[0]==target:
+                    return step+1
+                if nxt[1] not in visited[nxt[0]]:
+                    visited[nxt[0]].add(nxt[1])
+                    q.append(nxt)
+                # R
+                nxt = (cur[0], -1 if cur[1]>0 else 1)
+                if nxt[1] not in visited[nxt[0]]:
+                    visited[nxt[0]].add(nxt[1])
+                    q.append(nxt)
+            step += 1
+        return -1
