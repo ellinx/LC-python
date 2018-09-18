43a819eb769d6a23fc3a8559ac08643a0dccb2f3
ellinx <ellin.xll@gmail.com>
2018-09-17 19:57:52 -0700

    Update Closest Binary Search Tree Value II.

--- ClosestBinarySearchTreeValueII.py
+++ ClosestBinarySearchTreeValueII.py
@@ -48,3 +48,78 @@ class Solution:
         for i in range(k):
             ret.append(heapq.heappop(minHeap)[1])
         return ret
+
+class Solution2:
+    def closestKValues(self, root, target, k):
+        """
+        :type root: TreeNode
+        :type target: float
+        :type k: int
+        :rtype: List[int]
+        """
+        def getPredecessor(stk):
+            cur = stk.pop()
+            ret = cur.val
+            cur = cur.left
+            while cur:
+                stk.append(cur)
+                cur = cur.right
+            return ret
+
+        def getSuccessor(stk):
+            cur = stk.pop()
+            ret = cur.val
+            cur = cur.right
+            while cur:
+                stk.append(cur)
+                cur = cur.left
+            return ret
+
+        stk1 = collections.deque()
+        cur = root
+        while cur:
+            if cur.val==target:
+                stk1.append(cur)
+                break
+            if cur.val<target:
+                stk1.append(cur)
+                cur = cur.right
+            else:
+                cur = cur.left
+        stk2 = collections.deque()
+        cur = root
+        while cur:
+            if cur.val==target:
+                stk2.append(cur)
+                break
+            if cur.val<target:
+                cur = cur.right
+            else:
+                stk2.append(cur)
+                cur = cur.left
+        ret = []
+        val1, val2 = None, None
+        for i in range(k):
+            if val1 is None and len(stk1):
+                val1 = getPredecessor(stk1)
+            if val2 is None and len(stk2):
+                val2 = getSuccessor(stk2)
+            #print(val1,val2)
+            if val1 is not None and val2 is not None:
+                if val1==val2:
+                    ret.append(val1)
+                    val1, val2 = None, None
+                    continue
+                if target-val1<val2-target:
+                    ret.append(val1)
+                    val1 = None
+                else:
+                    ret.append(val2)
+                    val2 = None
+            elif val1 is not None:
+                ret.append(val1)
+                val1 = None
+            elif val2 is not None:
+                ret.append(val2)
+                val2 = None
+        return ret
