289035637370d12e565aaf54bcfd2f7e7072dfd1
ellinx <ellin.xll@gmail.com>
2018-10-04 10:44:17 +0800

    Update Climbing Stairs.

--- ClimbingStairs.py
+++ ClimbingStairs.py
@@ -30,11 +30,15 @@ class Solution:
         :type n: int
         :rtype: int
         """
-        dp = [1]*(n+1)
-        for i in range(1,n+1):
-            dp[i] = 0
-            if i-1>=0:
-                dp[i] += dp[i-1]
-            if i-2>=0:
-                dp[i] += dp[i-2]
-        return dp[n]
+        class Solution:
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a, b = 1, 2
+        if n==1:
+            return a
+        for i in range(3,n+1):
+            a, b = b, a+b
+        return b
