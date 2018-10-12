1f2a58a380f3b24acc5335c1594d884d2d0edfec
ellinx <ellin.xll@gmail.com>
2018-10-12 10:47:22 +0800

    Update Reverse Linked List.

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -9,33 +9,37 @@ Output: 5->4->3->2->1->NULL
 Follow up:
 A linked list can be reversed either iteratively or recursively. Could you implement both?
 """
-class ReverseLinkedList:
+class Solution:
     def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        # recursive
-        def helper(root):
-            #print(root.val)
-            if not root.next:
-                nonlocal ret
-                ret = root
-                return root
-            helper(root.next).next = root
-            root.next = None
-            return root
-        if not head:
-            return head
-        ret = None
-        helper(head)
-        return ret
-
         # iterative
-        # pre = None
-        # cur = head
-        # while cur:
-        #     nextCur = cur.next
-        #     cur.next = pre
-        #     pre, cur = cur, nextCur
-        # return pre
+        pre = None
+        cur = head
+        while cur:
+            nextCur = cur.next
+            cur.next = pre
+            pre, cur = cur, nextCur
+        return pre
+
+class Solution2:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        #recursive
+        def helper(head):
+            pre, cur = head, head.next
+            if cur is None:
+                return [head,head]
+            rh, rt = helper(cur)
+            rt.next = pre
+            pre.next = None
+            return [rh, pre]
+
+        if head is None:
+            return None
+        return helper(head)[0]
