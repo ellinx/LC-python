039452505562b895f469f53a76e438f5738ac00b
ellinx <ellin.xll@gmail.com>
2018-10-19 11:18:18 +0800

    Update Diameter of Binary Tree.

--- DiameterOfBinaryTree.py
+++ DiameterOfBinaryTree.py
@@ -14,7 +14,6 @@ Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
 
 Note: The length of path between two nodes is represented by the number of edges between them.
 """
-
 class Solution:
     # use a recursive helper function that takes a TreeNode root as input
     # update longest path with a new candidate which is longest path ends at left child, root and longest path ends at right child
@@ -26,15 +25,15 @@ class Solution:
         """
         def helper(root):
             if root is None:
-                return 0
+                return [0, 0]
             l = helper(root.left)
             r = helper(root.right)
-            nonlocal ret
-            ret = max(ret, l+r+1)
-            return max(l,r)+1
-
-        ret = 0
-        helper(root)
-        if ret==0:
+            ret = [0,0]
+            ret[0] = max(l[0],r[0])+1
+            ret[1] = max(l[1], r[1], l[0]+r[0]+1)
             return ret
+
+        ret = helper(root)[1]
+        if ret==0:
+            return 0
         return ret-1
