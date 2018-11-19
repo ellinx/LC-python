3fb75b1fadc148f3b5fee3dfd066e71fa807a5d4
Unknown <ellin.xll@gmail.com>
2018-11-18 19:56:42 -0800

    Update First Bad Version.

--- FirstBadVersion.py
+++ FirstBadVersion.py
@@ -24,20 +24,19 @@ Then 4 is the first bad version.
 # @return a bool
 # def isBadVersion(version):
 
-class Solution(object):
+class Solution:
     def firstBadVersion(self, n):
         """
         :type n: int
         :rtype: int
         """
-        start, end = 1,n
-        while start<end:
-            mid = start+(end-start)//2
-            if isBadVersion(mid):
-                if mid-1>=0 and isBadVersion(mid-1):
-                    end = mid-1
-                else:
-                    return mid
+        l, r = 1, n
+        while l<=r:
+            m = l+(r-l)//2
+            if isBadVersion(m):
+                if m-1<l or (not isBadVersion(m-1)):
+                    return m
+                r = m-1
             else:
-                start = mid+1
-        return start
+                l = m+1
+        return 0
