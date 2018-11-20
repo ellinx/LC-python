dc0be5ba2c696dd6c4f1430cfa18b4c12ebcaf0f
ellinx <ellin.xll@gmail.com>
2018-11-19 19:39:24 -0800

    Update Isomorphic Strings.

--- IsomorphicStrings.py
+++ IsomorphicStrings.py
@@ -24,7 +24,7 @@ Output: true
 Note:
 You may assume both s and t have the same length.
 """
-class IsomorphicStrings:
+class Solution:
     def isIsomorphic(self, s, t):
         """
         :type s: str
@@ -33,16 +33,15 @@ class IsomorphicStrings:
         """
         if len(s)!=len(t):
             return False
-        replace_map = dict()
-        index = 0
-        while index<len(s):
-            if s[index] not in replace_map:
-                if t[index] in replace_map.values():
+        mm = dict()
+        used = set()
+        for i in range(len(s)):
+            if s[i] not in mm:
+                if t[i] in used:
                     return False
-                replace_map[s[index]] = t[index]
-                index += 1
+                mm[s[i]] = t[i]
+                used.add(t[i])
                 continue
-            if replace_map[s[index]] != t[index]:
+            if mm[s[i]]!=t[i]:
                 return False
-            index += 1
         return True
