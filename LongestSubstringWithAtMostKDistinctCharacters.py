fa739c6dbf3a2d4315ad43548f650bf725a6285b
ellinx <ellin.xll@gmail.com>
2018-12-04 21:08:33 -0800

    Update Longest Substring with At Most K Distinct Characters.

--- LongestSubstringWithAtMostKDistinctCharacters.py
+++ LongestSubstringWithAtMostKDistinctCharacters.py
@@ -3,7 +3,7 @@
 
 For example, Given s = “eceba” and k = 2,
 
-T is "ece" which its length is 3. 
+T is "ece" which its length is 3.
 """
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s, k):
@@ -12,19 +12,16 @@ class Solution:
         :type k: int
         :rtype: int
         """
-        counter = dict()
-        start, end = 0, 0
         ret = 0
-        while end<len(s):
-            if s[end] not in counter:
-                counter[s[end]] = 0
-            counter[s[end]] += 1
+        counter = dict()
+        l, r = 0, 0
+        while r<len(s):
+            counter[s[r]] = counter.get(s[r], 0)+1
+            r += 1
             while len(counter)>k:
-                if counter[s[start]]==1:
-                    counter.pop(s[start])
-                else:
-                    counter[s[start]] -= 1
-                start += 1
-            ret = max(ret, end-start+1)
-            end += 1
+                counter[s[l]] -= 1
+                if counter[s[l]]==0:
+                    counter.pop(s[l])
+                l += 1
+            ret = max(ret, r-l)
         return ret
