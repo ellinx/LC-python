a0a06f5af0eed07427a87ad5e62cb259f51b4a5b
ellinx <ellin.xll@gmail.com>
2018-12-18 19:47:21 -0800

    Update Find Median from Data Stream.

--- FindMedianFromDataStream.py
+++ FindMedianFromDataStream.py
@@ -1,5 +1,7 @@
 """
-Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. So the median is the mean of the two middle value.
+Median is the middle value in an ordered integer list.
+If the size of the list is even, there is no middle value.
+So the median is the mean of the two middle value.
 
 For example,
 [2,3,4] , the median is 3
@@ -10,6 +12,7 @@ Design a data structure that supports the following two operations:
 
 void addNum(int num) - Add a integer number from the data stream to the data structure.
 double findMedian() - Return the median of all elements so far.
+
 Example:
 
 addNum(1)
