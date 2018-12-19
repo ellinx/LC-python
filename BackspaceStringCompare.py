921df4a646fe1a4339fc96a228641a530ebf6518
ellinx <ellin.xll@gmail.com>
2018-12-18 19:53:58 -0800

    Update Backspace String Compare.

--- BackspaceStringCompare.py
+++ BackspaceStringCompare.py
@@ -1,5 +1,6 @@
 """
-Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+Given two strings S and T, return if they are equal when both are typed into empty text editors.
+# means a backspace character.
 
 Example 1:
 
