32d4401ffe4d2f856e17c23a34299660875b1301
ellinx <ellin.xll@gmail.com>
2018-12-27 21:08:22 -0800

    Update Reconstruct Itinerary.

--- ReconstructItinerary.py
+++ ReconstructItinerary.py
@@ -1,24 +1,25 @@
 """
-Given a list of airline tickets represented by pairs of departure and arrival airports [from, to], reconstruct the itinerary in order.
-All of the tickets belong to a man who departs from JFK. Thus, the itinerary must begin with JFK.
+Given a list of airline tickets represented by pairs of departure and arrival airports [from, to],
+reconstruct the itinerary in order. All of the tickets belong to a man who departs from JFK.
+Thus, the itinerary must begin with JFK.
 
 Note:
 
-1. If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string.
+1. If there are multiple valid itineraries, you should return the itinerary
+    that has the smallest lexical order when read as a single string.
     For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
 2. All airports are represented by three capital letters (IATA code).
 3. You may assume all tickets form at least one valid itinerary.
 
 Example 1:
-
 Input: tickets = [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
 Output: ["JFK", "MUC", "LHR", "SFO", "SJC"]
 
 Example 2:
-
 Input: tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
 Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
-Explanation: Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"]. But it is larger in lexical order.
+Explanation: Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"].
+                But it is larger in lexical order.
 
 """
 class Solution:
@@ -27,30 +28,24 @@ class Solution:
         :type tickets: List[List[str]]
         :rtype: List[str]
         """
-        def dfs(g, cur, ticketsLeft):
-            #print(ticketsLeft,cur)
-            if ticketsLeft==0:
-                nonlocal ret
-                ret = cur
-                return True
-            for each in sorted(list(g[cur[-1]])):
-                if g[cur[-1]][each]==1:
-                    g[cur[-1]].pop(each)
-                else:
-                    g[cur[-1]][each] -= 1
-                if dfs(g, cur+[each],ticketsLeft-1):
-                    return True
-                if each not in g[cur[-1]]:
-                    g[cur[-1]][each] = 0
-                g[cur[-1]][each] += 1
-            return False
+        def dfs(g, last, cur, total):
+            if total==0:
+                return cur
+            for i in range(len(g[last])):
+                if i-1>=0 and g[last][i-1]==g[last][i]:
+                    continue
+                tmp = g[last][i]
+                g[last] = g[last][:i]+g[last][i+1:]
+                ret = dfs(g, tmp, cur+[tmp], total-1)
+                if ret is not None:
+                    return ret
+                g[last] = g[last][:i]+[tmp]+g[last][i:]
+            return None
 
-        g = collections.defaultdict(dict)
-        for ticket in tickets:
-            if ticket[1] not in g[ticket[0]]:
-                g[ticket[0]][ticket[1]] = 0
-            g[ticket[0]][ticket[1]] += 1
+        g = collections.defaultdict(list)
+        for begin, end in tickets:
+            g[begin].append(end)
+        for k in g:
+            g[k].sort()
         #print(g)
-        ret = []
-        dfs(g,["JFK"], len(tickets))
-        return ret
+        return dfs(g, "JFK", ["JFK"], len(tickets))
