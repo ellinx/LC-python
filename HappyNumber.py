a0040aee98b022ee42c6fd97abfe0a5a0a450c68
ellinx <ellin.xll@gmail.com>
2018-12-31 19:48:05 -0800

    Update Happy Number.

--- HappyNumber.py
+++ HappyNumber.py
@@ -1,7 +1,9 @@
 """
 Write an algorithm to determine if a number is "happy".
 
-A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+A happy number is a number defined by the following process: Starting with any positive integer,
+replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay),
+or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
 
 Example: 19 is a happy number
 
@@ -10,7 +12,6 @@ Example: 19 is a happy number
 6^2 + 8^2 = 100
 1^2 + 0^2 + 0^2 = 1
 """
-
 class Solution:
     def isHappy(self, n):
         """
@@ -18,16 +19,36 @@ class Solution:
         :rtype: bool
         """
         visited = set()
-        while n not in visited:
+        while n!=1 and n not in visited:
             visited.add(n)
             nxt = 0
             for c in str(n):
                 nxt += int(c)**2
-            if nxt==1:
-                return True
             n = nxt
+        if n==1:
+            return True
         return False
 
+class Solution2:
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        def getNext(n):
+            ret = 0
+            for c in str(n):
+                ret += int(c)**2
+            return ret
+
+        fast, slow = n, n
+        while True:
+            slow = getNext(slow)
+            fast = getNext(fast)
+            fast = getNext(fast)
+            if fast==slow:
+                break
+        return fast==1
 
 # test
 if __name__ == "__main__":
