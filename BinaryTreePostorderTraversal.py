e6a95fa376113224257ad464304ef4c9992202ee
ellinx <ellin.xll@gmail.com>
2018-12-31 19:39:38 -0800

    Update Binary Tree Postorder Traversal.

--- BinaryTreePostorderTraversal.py
+++ BinaryTreePostorderTraversal.py
@@ -31,3 +31,26 @@ class Solution:
         ret = []
         dfs(root,ret)
         return ret
+
+
+class Solution2:
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        ret = []
+        stk = collections.deque()
+        while root is not None or len(stk):
+            if root is not None:
+                stk.append([root,0])
+                root = root.left
+            else:
+                root, r = stk.pop()
+                if r==1:
+                    ret.append(root.val)
+                    root = None
+                else:
+                    stk.append([root, 1])
+                    root = root.right
+        return ret
