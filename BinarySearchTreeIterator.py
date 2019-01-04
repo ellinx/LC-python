88ebdd9719b88adf6d1534d62f6a3dc9583329ea
ellinx <ellin.xll@gmail.com>
2019-01-03 22:03:44 -0800

    Update Binary Search Tree Iterator.

--- BinarySearchTreeIterator.py
+++ BinarySearchTreeIterator.py
@@ -35,6 +35,48 @@ class BSTIterator(object):
             cur = cur.left
         return ret.val
 
+class BSTIterator2:
+    """
+    Morris traverse
+    """
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.pre, self.cur = None, root
+        self.hasNextCalled = False
+
+    def next(self):
+        """
+        @return the next smallest number
+        :rtype: int
+        """
+        if not self.hasNextCalled:
+            self.hasNext()
+        ret = self.cur.val
+        self.cur = self.cur.right
+        return ret
+
+    def hasNext(self):
+        """
+        @return whether we have a next smallest number
+        :rtype: bool
+        """
+        while self.cur is not None:
+            if self.cur.left is None:
+                break
+            self.pre = self.cur.left
+            while self.pre.right is not None and self.pre.right!=self.cur:
+                self.pre = self.pre.right
+            if self.pre.right is None:
+                self.pre.right = self.cur
+                self.cur = self.cur.left
+            else:
+                self.pre.right = None
+                break
+        self.hasNextCalled = True
+        return self.cur is not None
+
 # Your BSTIterator will be called like this:
 # i, v = BSTIterator(root), []
 # while i.hasNext(): v.append(i.next())
