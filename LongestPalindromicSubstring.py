f6fa2a59e17eb1b97ff9c7ab846a2ebc3104a602
ellinx <ellin.xll@gmail.com>
2019-01-07 19:28:53 -0800

    Update Longest Palindromic Substring.

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -1,13 +1,13 @@
 """
-Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+Given a string s, find the longest palindromic substring in s.
+You may assume that the maximum length of s is 1000.
 
 Example 1:
-
 Input: "babad"
 Output: "bab"
 Note: "aba" is also a valid answer.
-Example 2:
 
+Example 2:
 Input: "cbbd"
 Output: "bb"
 """
@@ -17,18 +17,28 @@ class Solution:
         :type s: str
         :rtype: str
         """
-        s = "#"+"#".join(s)+"#"
-        #print(s)
+        n = len(s)
         ret = ""
-        for i in range(len(s)):
-            offset = 0
-            while i-offset>=0 and i+offset<len(s) and s[i-offset]==s[i+offset]:
-                offset += 1
-            offset -= 1
-            if s[i]=='#' and len(ret)<(offset//2)*2:
-                #print(i, s[i-offset+1:i+offset])
-                ret = "".join(s[i-offset+1:i+offset].split("#"))
-            elif s[i]!='#' and len(ret)<(offset//2)*2+1:
-                #print(i, s[i-offset+1:i+offset])
-                ret = "".join(s[i-offset+1:i+offset].split("#"))
+        for i in range(n):
+            if len(ret)>=1+2*(n-i-1):
+                break
+            # odd
+            l, r = i-1, i+1
+            while l>=0 and r<n and s[l]==s[r]:
+                l -= 1
+                r += 1
+            l += 1
+            r -= 1
+            if len(ret)<r-l+1:
+                ret = s[l:r+1]
+            # even
+            if i+1<n and s[i]==s[i+1]:
+                l, r = i, i+1
+                while l>=0 and r<n and s[l]==s[r]:
+                    l -= 1
+                    r += 1
+                l += 1
+                r -= 1
+                if len(ret)<r-l+1:
+                    ret = s[l:r+1]
         return ret
