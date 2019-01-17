19e171d1c857ba8cccf75689fb741624629df078
ellinx <ellin.xll@gmail.com>
2019-01-16 23:39:37 -0800

    Add Set Matrix Zeroes.

---
+++ SetMatrixZeroes.py
@@ -0,0 +1,57 @@
+"""
+Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
+
+Example 1:
+Input:
+[
+  [1,1,1],
+  [1,0,1],
+  [1,1,1]
+]
+Output:
+[
+  [1,0,1],
+  [0,0,0],
+  [1,0,1]
+]
+
+Example 2:
+Input:
+[
+  [0,1,2,0],
+  [3,4,5,2],
+  [1,3,1,5]
+]
+Output:
+[
+  [0,0,0,0],
+  [0,4,5,0],
+  [0,3,1,0]
+]
+
+Follow up:
+1. A straight forward solution using O(mn) space is probably a bad idea.
+2. A simple improvement uses O(m + n) space, but still not the best solution.
+3. Could you devise a constant space solution?
+"""
+class Solution:
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix)
+        if m==0:
+            return
+        n = len(matrix[0])
+        rows = set()
+        cols = set()
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j]==0:
+                    rows.add(i)
+                    cols.add(j)
+        for i in range(m):
+            for j in range(n):
+                if i in rows or j in cols:
+                    matrix[i][j] = 0
