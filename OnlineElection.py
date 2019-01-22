7da36961d6008f5965da228e728dfa40531b9e50
ellinx <ellin.xll@gmail.com>
2019-01-21 22:20:08 -0800

    Update Online Election.

--- OnlineElection.py
+++ OnlineElection.py
@@ -7,9 +7,7 @@ TopVotedCandidate.q(int t) will return the number of the person that was leading
 Votes cast at time t will count towards our query.  In the case of a tie, the most recent vote (among tied candidates) wins.
 
 
-
 Example 1:
-
 Input: ["TopVotedCandidate","q","q","q","q","q","q"], [[[0,1,1,0,0,1,0],[0,5,10,15,20,25,30]],[3],[12],[25],[15],[24],[8]]
 Output: [null,0,1,1,0,0,1]
 Explanation:
@@ -20,12 +18,11 @@ This continues for 3 more queries at time 15, 24, and 8.
 
 
 Note:
-
-1 <= persons.length = times.length <= 5000
-0 <= persons[i] <= persons.length
-times is a strictly increasing array with all elements in [0, 10^9].
-TopVotedCandidate.q is called at most 10000 times per test case.
-TopVotedCandidate.q(int t) is always called with t >= times[0].
+1. 1 <= persons.length = times.length <= 5000
+2. 0 <= persons[i] <= persons.length
+3. times is a strictly increasing array with all elements in [0, 10^9].
+4. TopVotedCandidate.q is called at most 10000 times per test case.
+5. TopVotedCandidate.q(int t) is always called with t >= times[0].
 """
 class TopVotedCandidate:
 
@@ -66,3 +63,27 @@ class TopVotedCandidate:
 # Your TopVotedCandidate object will be instantiated and called as such:
 # obj = TopVotedCandidate(persons, times)
 # param_1 = obj.q(t)
+
+
+class TopVotedCandidate2:
+
+    def __init__(self, persons, times):
+        """
+        :type persons: List[int]
+        :type times: List[int]
+        """
+        self.votes = collections.defaultdict(list)
+        for i in range(len(persons)):
+            self.votes[persons[i]].append(times[i])
+
+    def q(self, t):
+        """
+        :type t: int
+        :rtype: int
+        """
+        pq = []
+        for p in self.votes:
+            idx = bisect.bisect_right(self.votes[p], t)
+            if idx>0:
+                heapq.heappush(pq, [-idx, -self.votes[p][idx-1], p])
+        return heapq.heappop(pq)[2]
