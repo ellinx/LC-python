65764da72da8c01574ec0e2d1aab5d692a503ad5
ellinx <ellin.xll@gmail.com>
2019-01-21 22:21:23 -0800

    Update Burst Balloons.

--- BurstBalloons.py
+++ BurstBalloons.py
@@ -1,15 +1,15 @@
 """
-Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by array nums. You are asked to burst all the balloons. If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins. Here left and right are adjacent indices of i. After the burst, the left and right then becomes adjacent.
+Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by array nums.
+You are asked to burst all the balloons. If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins.
+Here left and right are adjacent indices of i. After the burst, the left and right then becomes adjacent.
 
 Find the maximum coins you can collect by bursting the balloons wisely.
 
 Note:
-
-    You may imagine nums[-1] = nums[n] = 1. They are not real therefore you can not burst them.
-    0 ≤ n ≤ 500, 0 ≤ nums[i] ≤ 100
+1. You may imagine nums[-1] = nums[n] = 1. They are not real therefore you can not burst them.
+2. 0 ≤ n ≤ 500, 0 ≤ nums[i] ≤ 100
 
 Example:
-
 Input: [3,1,5,8]
 Output: 167
 Explanation: nums = [3,1,5,8] --> [3,5,8] -->   [3,8]   -->  [8]  --> []
@@ -23,18 +23,18 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        #use last burst ballon instead of first burst balloon
-        def dfs(nums, left, right, mem):
-            if right-left==1:
+        def dfs(B, l, r, mm):
+            if l+1==r:
                 return 0
-            if mem[left][right]:
-                return mem[left][right]
+            if (l,r) in mm:
+                return mm[(l,r)]
             ret = 0
-            for i in range(left+1,right):
-                ret = max(ret, nums[left]*nums[i]*nums[right]+dfs(nums,left,i,mem)+dfs(nums,i,right,mem))
-            mem[left][right] = ret
+            for i in range(l+1,r):
+                temp = B[l]*B[i]*B[r]
+                temp += dfs(B,l,i,mm)+dfs(B,i,r,mm)
+                ret = max(ret, temp)
+            mm[(l,r)] = ret
             return ret
 
-        nums = [1]+nums+[1]
-        mem = [[0]*len(nums) for _ in range(len(nums))]
-        return dfs(nums, 0, len(nums)-1, mem)
+        B = [1]+nums+[1]
+        return dfs(B,0,len(B)-1, dict())
