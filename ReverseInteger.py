76c95312ebc4b58ff6c47f72cc49c040d24ee2cc
ellinx <ellin.xll@gmail.com>
2019-01-21 22:14:15 -0800

    Update Reverse Integer.

--- ReverseInteger.py
+++ ReverseInteger.py
@@ -2,22 +2,20 @@
 Given a 32-bit signed integer, reverse digits of an integer.
 
 Example 1:
-
 Input: 123
 Output: 321
 
 Example 2:
-
 Input: -123
 Output: -321
 
 Example 3:
-
 Input: 120
 Output: 21
 
 Note:
-Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1].
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range:
+[−2^31,  2^31 − 1].
 For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 
 """
@@ -32,5 +30,27 @@ class Solution:
             x = -x
         ret = sign*int(str(x)[::-1])
         if ret>2**31-1 or ret<-2**31:
+            return 0
+        return ret
+
+class Solution2:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x<0:
+            ret = self.reverse(-x)
+            if ret>2**31:
+                return 0
+            return -ret
+        stk = collections.deque()
+        while x>0:
+            stk.append(x%10)
+            x //= 10
+        ret = 0
+        while len(stk)>0:
+            ret = ret*10+stk.popleft()
+        if ret>2**31-1:
             return 0
         return ret
