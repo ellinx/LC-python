6de38f9ce7a14905bf7f924039ad977ffef13c8a
ellinx <ellin.xll@gmail.com>
2019-01-23 20:23:26 -0800

    Update Recover Binary Search Tree.

--- RecoverBinarySearchTree.py
+++ RecoverBinarySearchTree.py
@@ -4,7 +4,6 @@ Two elements of a binary search tree (BST) are swapped by mistake.
 Recover the tree without changing its structure.
 
 Example 1:
-
 Input: [1,3,null,null,2]
 
    1
@@ -22,7 +21,6 @@ Output: [3,1,null,null,2]
    2
 
 Example 2:
-
 Input: [3,1,4,null,null,2]
 
   3
@@ -40,12 +38,18 @@ Output: [2,1,4,null,null,3]
   3
 
 Follow up:
-
-    A solution using O(n) space is pretty straight forward.
-    Could you devise a constant space solution?
+1. A solution using O(n) space is pretty straight forward.
+2. Could you devise a constant space solution?
 
 
 """
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
     def recoverTree(self, root):
         """
@@ -57,12 +61,13 @@ class Solution:
                 return
             dfs(root.left, swap)
             nonlocal pre
-            if pre and pre.val>root.val:
+            if pre is not None and pre.val>root.val:
                 if swap[0] is None:
                     swap[0] = pre
                     swap[1] = root
                 else:
                     swap[1] = root
+                    return
             pre = root
             dfs(root.right, swap)
 
