9a70c25500df939c6e18e879ac035dbf70a3cd23
ellinx <ellin.xll@gmail.com>
2019-01-24 19:50:36 -0800

    Add Cracking the Safe.

---
+++ CrackingTheSafe.py
@@ -0,0 +1,54 @@
+"""
+There is a box protected by a password. The password is n digits,
+where each letter can be one of the first k digits 0, 1, ..., k-1.
+
+You can keep inputting the password, the password will automatically be matched against the last n digits entered.
+
+For example, assuming the password is "345", I can open it when I type "012345", but I enter a total of 6 digits.
+
+Please return any string of minimum length that is guaranteed to open the box after the entire string is inputted.
+
+Example 1:
+Input: n = 1, k = 2
+Output: "01"
+Note: "10" will be accepted too.
+
+Example 2:
+Input: n = 2, k = 2
+Output: "00110"
+Note: "01100", "10011", "11001" will be accepted too.
+
+Note:
+1. n will be in the range [1, 4].
+2. k will be in the range [1, 10].
+3. k^n will be at most 4096.
+"""
+class Solution:
+    def crackSafe(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        def dfs(n, k, cur, pw, checked):
+            nonlocal ret
+            if len(checked)==k**n:
+                ret = cur
+                return True
+            if len(pw)==n:
+                pw = pw[1:]
+            for i in range(k):
+                nxt = pw+str(i)
+                if len(nxt)<n:
+                    if dfs(n, k, cur+str(i), nxt, checked):
+                        return True
+                    continue
+                if nxt not in checked:
+                    checked.add(nxt)
+                    if dfs(n, k, cur+str(i), nxt, checked):
+                        return True
+                    checked.remove(nxt)
+
+        ret = None
+        dfs(n, k, "", "", set())
+        return ret
