0245b7800a35943b432b6f1079d734a42dae9126
ellinx <ellin.xll@gmail.com>
2019-01-24 19:52:03 -0800

    Update Search in Rotated Sorted Array.

--- SearchInRotatedSortedArray.py
+++ SearchInRotatedSortedArray.py
@@ -46,3 +46,37 @@ class Solution:
                 else:
                     end = mid-1
         return -1
+
+
+class Solution2:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        def bSearch(nums, target, l, r):
+            while l<=r:
+                m = l+(r-l)//2
+                if nums[m]==target:
+                    return m
+                if nums[m]<target:
+                    l = m+1
+                else:
+                    r = m-1
+            return -1
+
+        if len(nums)==0:
+            return -1
+        l, r = 0, len(nums)-1
+        while l<r:
+            m = l+(r-l)//2
+            if nums[m]>nums[r]:
+                l = m+1
+            else:
+                r = m
+        idx = l
+        #print(idx)
+        if target>nums[-1]:
+            return bSearch(nums, target, 0, idx)
+        return bSearch(nums, target, idx, len(nums))
