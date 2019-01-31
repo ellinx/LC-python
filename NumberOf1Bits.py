56edfc34be364a7b2c6f883253551f69407be16f
ellinx <ellin.xll@gmail.com>
2019-01-30 19:25:09 -0800

    Update Number of 1 Bits.

--- NumberOf1Bits.py
+++ NumberOf1Bits.py
@@ -2,13 +2,11 @@
 Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
 
 Example 1:
-
 Input: 11
 Output: 3
 Explanation: Integer 11 has binary representation 00000000000000000000000000001011
 
 Example 2:
-
 Input: 128
 Output: 1
 Explanation: Integer 128 has binary representation 00000000000000000000000010000000
@@ -22,8 +20,8 @@ class Solution(object):
         :rtype: int
         """
         ret = 0
-        while n:
-            if n&1:
+        while n>0:
+            if (n&1)==1:
                 ret += 1
-            n >>= 1
+            n = n>>1
         return ret
