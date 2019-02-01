0720eae06197c3361ef5c4abe8aaac75e7f7399f
ellinx <ellin.xll@gmail.com>
2019-01-31 20:42:23 -0800

    Update Longest Substring Without Repeating Characters.

--- LongestSubstringWithoutRepeatingCharacters.py
+++ LongestSubstringWithoutRepeatingCharacters.py
@@ -8,6 +8,7 @@ Given "abcabcbb", the answer is "abc", which the length is 3.
 Given "bbbbb", the answer is "b", with the length of 1.
 
 Given "pwwkew", the answer is "wke", with the length of 3.
+
 Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
 """
@@ -18,13 +19,13 @@ class Solution:
         :rtype: int
         """
         ret = 0
-        charSet = set()
-        start, end = 0, 0
-        while end<len(s):
-            while s[end] in charSet:
-                charSet.remove(s[start])
-                start += 1
-            charSet.add(s[end])
-            end += 1
-            ret = max(ret, end-start)
+        counter = collections.defaultdict(int)
+        l, r = 0, 0
+        while r<len(s):
+            counter[s[r]] += 1
+            while counter[s[r]]>1:
+                counter[s[l]] -= 1
+                l += 1
+            r += 1
+            ret = max(ret, r-l)
         return ret
