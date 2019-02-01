10f17912dabc89526ecf2c638671147f63e02266
ellinx <ellin.xll@gmail.com>
2019-01-31 20:48:48 -0800

    Update Minimum Path Sum.

--- MinimumPathSum.py
+++ MinimumPathSum.py
@@ -22,21 +22,18 @@ class Solution:
         :rtype: int
         """
         m = len(grid)
-        if m == 0:
+        if m==0:
             return 0
         n = len(grid[0])
-        cur = [0]*n
+        dp = [[0]*n for _ in range(m)]
         for i in range(n):
             if i==0:
-                cur[i] = grid[0][i]
+                dp[0][0] = grid[0][0]
                 continue
-            cur[i] = cur[i-1]+grid[0][i]
+            dp[0][i] = dp[0][i-1]+grid[0][i]
         for i in range(1,m):
-            nxt = [0]*n
-            for j in range(n):
-                if j == 0:
-                    nxt[j] = cur[j]+grid[i][j]
-                    continue
-                nxt[j] = min(cur[j], nxt[j-1])+grid[i][j]
-            cur = nxt
-        return cur[-1]
+            dp[i][0] = dp[i-1][0]+grid[i][0]
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1])+grid[i][j]
+        return dp[-1][-1]
