7933c447bcbc4b823270e26e333c370f0b0195f2
ellinx <ellin.xll@gmail.com>
2019-01-31 20:47:03 -0800

    Update Reverse Linked List.

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -9,37 +9,39 @@ Output: 5->4->3->2->1->NULL
 Follow up:
 A linked list can be reversed either iteratively or recursively. Could you implement both?
 """
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution:
     def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        # iterative
-        pre = None
-        cur = head
-        while cur:
-            nextCur = cur.next
-            cur.next = pre
-            pre, cur = cur, nextCur
-        return pre
+        if head is None:
+            return None
+        p0, p1, p2 = None, head, head.next
+        while p2 is not None:
+            p3 = p2.next
+            p1.next, p2.next = p0, p1
+            p0, p1, p2 = p1, p2, p3
+        return p1
 
 class Solution2:
+    #recursive
     def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        #recursive
-        def helper(head):
-            pre, cur = head, head.next
-            if cur is None:
-                return [head,head]
-            rh, rt = helper(cur)
-            rt.next = pre
-            pre.next = None
-            return [rh, pre]
-
         if head is None:
             return None
-        return helper(head)[0]
+        if head.next is None:
+            return head
+        p1, p2 = head, head.next
+        ret = self.reverseList(p2)
+        p2.next, p1.next = p1, None
+        return ret
