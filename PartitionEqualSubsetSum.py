1c3ad322386e81843d559c5cffbb8b8819170bca
ellinx <ellin.xll@gmail.com>
2019-02-05 21:59:11 -0800

    Update Partition Equal Subset Sum.

--- PartitionEqualSubsetSum.py
+++ PartitionEqualSubsetSum.py
@@ -1,52 +1,43 @@
 """
-Given a non-empty array containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+Given a non-empty array containing only positive integers,
+find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
 
 Note:
-Each of the array element will not exceed 100.
-The array size will not exceed 200.
-Example 1:
+1. Each of the array element will not exceed 100.
+2. The array size will not exceed 200.
 
+Example 1:
 Input: [1, 5, 11, 5]
 Output: true
-
 Explanation: The array can be partitioned as [1, 5, 5] and [11].
 
 Example 2:
 Input: [1, 2, 3, 5]
 Output: false
-
 Explanation: The array cannot be partitioned into equal sum subsets.
 """
-
-
-class PartitionEqualSubsetSum:
+class Solution:
     def canPartition(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
-        # 0/1 knapsack
         total = sum(nums)
-        if total%2:
+        if total%2==1:
             return False
-        total //= 2
-        cur = [False]*(total+1)
-        cur[0] = True
-        if nums[0]<=total:
-            cur[nums[0]] = True
-        for i in range(1,len(nums)):
-            nxt = [False]*(total+1)
-            nxt[0] = True
-            for j in range(1,total+1):
-                # dont take ith
-                nxt[j] = cur[j]
-                # take ith
-                if j>=nums[i]:
-                    nxt[j] = nxt[j] or cur[j-nums[i]]
-            if nxt[-1]:
-                return True
-            cur = nxt
-        return False
+        target = total//2
+        # dp[i][j] means if first i numbers in nums can sum to j
+        dp = [[False]*(target+1) for _ in range(len(nums)+1)]
+        for i in range(len(nums)+1):
+            dp[i][0] = True
+        for i in range(1, len(nums)+1):
+            for j in range(1, target+1):
+                # if not take ith number
+                dp[i][j] = dp[i-1][j]
+                # if take ith number
+                if j-nums[i-1]>=0:
+                    dp[i][j] = dp[i][j] or dp[i-1][j-nums[i-1]]
+        return dp[len(nums)][target]
 
 
 if __name__=="__main__":
