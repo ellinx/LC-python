d0932c456e5f384d5b2d7aa3ed5ade959e4ea648
ellinx <ellin.xll@gmail.com>
2019-02-06 22:03:25 -0800

    Update Basic Calculator.

--- BasicCalculator.py
+++ BasicCalculator.py
@@ -1,7 +1,8 @@
 """
 Implement a basic calculator to evaluate a simple expression string.
 
-The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+The expression string may contain open ( and closing parentheses ),
+the plus + or minus sign -, non-negative integers and empty spaces .
 
 Example 1:
 Input: "1 + 1"
@@ -20,36 +21,35 @@ You may assume that the given expression is always valid.
 Do not use the eval built-in library function.
 """
 class Solution:
-    def calculate(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+    def calculate(self, s: 'str') -> 'int':
         stk = collections.deque()
-        ret = 0
+        cur = 0
         sign = 1
-        index = 0
-        while index<len(s):
-            if s[index]==" ":
-                index += 1
+        idx = 0
+        while idx<len(s):
+            if s[idx]==" ":
+                idx += 1
                 continue
-            if s[index]=="+" or s[index]=="-":
-                sign = 1 if s[index]=="+" else -1
-                index += 1
-            elif s[index]=="(":
-                stk.append(ret)
-                stk.append(sign)
-                ret, sign = 0, 1
-                index += 1
-            elif s[index]==")":
-                sign = stk.pop()
-                ret = stk.pop()+sign*ret
-                index += 1
-            else:
-                end = index+1
+            if s[idx]=="(":
+                stk.append([cur,sign])
+                cur, sign = 0, 1
+                idx += 1
+            elif s[idx]==")":
+                temp = cur
+                cur, sign = stk.pop()
+                cur += sign*temp
+                sign = 1
+                idx += 1
+            elif s[idx]=="+":
+                idx += 1
+            elif s[idx]=="-":
+                sign = -1
+                idx += 1
+            elif s[idx].isdigit():
+                end = idx+1
                 while end<len(s) and s[end].isdigit():
                     end += 1
-                ret += sign*int(s[index:end])
-                index = end
-            #print(stk,ret)
-        return ret
+                cur += sign*int(s[idx:end])
+                sign = 1
+                idx = end
+        return cur
