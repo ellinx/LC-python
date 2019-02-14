bac5fd592b358cfb1a341216a833455b6eb0d34d
ellinx <ellin.xll@gmail.com>
2019-02-13 20:19:41 -0800

    Update Plus One.

--- PlusOne.py
+++ PlusOne.py
@@ -1,7 +1,9 @@
 """
-Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+Given a non-empty array of digits representing a non-negative integer,
+plus one to the integer.
 
-The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+The digits are stored such that the most significant digit is at the head of the list,
+and each element in the array contain a single digit.
 
 You may assume the integer does not contain any leading zero, except the number 0 itself.
 
@@ -18,18 +20,13 @@ Explanation: The array represents the integer 4321.
 
 """
 class Solution:
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
+    def plusOne(self, digits: 'List[int]') -> 'List[int]':
+        ret = []
         c = 1
         for i in range(len(digits)-1,-1,-1):
-            s = digits[i]+c
-            digits[i] = s%10
-            c = s//10
-            if c==0:
-                break
-        if c==0:
-            return digits
-        return [c]+digits
+            total = digits[i]+c
+            c = total//10
+            ret.append(total%10)
+        if c>0:
+            ret.append(c)
+        return ret[::-1]
