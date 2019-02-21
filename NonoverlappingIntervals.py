dec256f349c4c09df6e1fc049565e69e53aca6d4
ellinx <ellin.xll@gmail.com>
2019-02-20 20:09:11 -0800

    Update Non-overlapping Intervals.

--- NonoverlappingIntervals.py
+++ NonoverlappingIntervals.py
@@ -9,19 +9,16 @@ Intervals like [1,2] and [2,3] have borders "touching" but they don't overlap ea
 Example 1:
 Input: [ [1,2], [2,3], [3,4], [1,3] ]
 Output: 1
-
 Explanation: [1,3] can be removed and the rest of intervals are non-overlapping.
 
 Example 2:
 Input: [ [1,2], [1,2], [1,2] ]
 Output: 2
-
 Explanation: You need to remove two [1,2] to make the rest of intervals non-overlapping.
 
 Example 3:
 Input: [ [1,2], [2,3] ]
 Output: 0
-
 Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
 """
 from Interval import *
@@ -32,18 +29,14 @@ from Interval import *
 #         self.end = e
 
 class Solution:
-    def eraseOverlapIntervals(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: int
-        """
-        if len(intervals)==0:
+    def eraseOverlapIntervals(self, intervals: 'List[Interval]') -> 'int':
+        if len(intervals)<=1:
             return 0
-        intervals.sort(key=lambda x:[x.end, x.start])
-        ret = 0
+        intervals.sort(key=lambda x:[x.end, -x.start])
         cur = intervals[0]
+        ret = 0
         for i in range(1, len(intervals)):
-            if cur.end<=intervals[i].start:
+            if intervals[i].start>=cur.end:
                 cur = intervals[i]
                 continue
             ret += 1
