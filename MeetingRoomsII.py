921553ccaa3f7945704c76ca9a68d18643ae3ac8
ellinx <ellin.xll@gmail.com>
2019-02-22 19:22:18 -0800

    Update Meeting Rooms II.

--- MeetingRoomsII.py
+++ MeetingRoomsII.py
@@ -1,5 +1,6 @@
 """
-Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
+find the minimum number of conference rooms required.
 
 Example 1:
 Input: [[0, 30],[5, 10],[15, 20]]
