4ff8773c7beb06d25dfd39179a6ed201c6b5921c
ellinx <ellin.xll@gmail.com>
2019-02-22 19:28:44 -0800

    Update Divide Two Integers.

--- DivideTwoIntegers.py
+++ DivideTwoIntegers.py
@@ -1,5 +1,6 @@
 """
-Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
+Given two integers dividend and divisor, divide two integers without using multiplication,
+division and mod operator.
 
 Return the quotient after dividing dividend by divisor.
 
@@ -14,43 +15,37 @@ Input: dividend = 7, divisor = -3
 Output: -2
 
 Note:
-Both dividend and divisor will be 32-bit signed integers.
-The divisor will never be 0.
-Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1].
-For the purpose of this problem, assume that your function returns 2^31 − 1 when the division result overflows.
+1. Both dividend and divisor will be 32-bit signed integers.
+2. The divisor will never be 0.
+3. Assume we are dealing with an environment which could only store integers
+    within the 32-bit signed integer range: [−2^31,  2^31 − 1].
+4. For the purpose of this problem, assume that your function returns 2^31 − 1 when the division result overflows.
 """
 class Solution:
-    def divide(self, dividend, divisor):
-        """
-        :type dividend: int
-        :type divisor: int
-        :rtype: int
-        """
-        max_int = 2**31-1
-        min_int = -2**31
-        sign = 1
+    def divide(self, dividend: 'int', divisor: 'int') -> 'int':
         if dividend==0:
             return 0
-        if (dividend>0 and divisor<0) or (dividend<0 and divisor>0):
-            sign = -1
-        dividend, divisor = abs(dividend), abs(divisor)
-        mm = [divisor]
-        while mm[-1]<dividend:
-            mm.append(mm[-1]<<1)
-        #print(mm)
+        MAX_INT, MIN_INT = 2**31-1, -2**31
+        #print(MAX_INT, MIN_INT)
+        sign = 1
+        if dividend<0:
+            dividend = -dividend
+            sign *= -1
+        if divisor<0:
+            divisor = -divisor
+            sign *= -1
         ret = 0
+        mm = [divisor]
         while dividend>=divisor:
+            while dividend>mm[-1]:
+                mm.append(mm[-1]*2)
             idx = bisect.bisect_left(mm, dividend)
-            if mm[idx]==dividend:
+            if dividend==mm[idx]:
                 ret += 2**idx
-                break
-            if idx==0:
-                break
-            ret += 2**(idx-1)
-            dividend -= mm[idx-1]
-        ret *= sign
-        if ret>max_int:
-            return max_int
-        if ret<min_int:
-            return min_int
-        return ret
+                dividend = 0
+            else:
+                ret += 2**(idx-1)
+                dividend -= mm[idx-1]
+            if sign*ret>MAX_INT or sign*ret<MIN_INT:
+                return MAX_INT
+        return sign*ret
