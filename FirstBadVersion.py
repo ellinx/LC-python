dfe66aa7d9399336c5c40182c8bd9f7e88f31a2d
ellinx <ellin.xll@gmail.com>
2019-02-22 19:26:27 -0800

    Update First Bad Version.

--- FirstBadVersion.py
+++ FirstBadVersion.py
@@ -1,9 +1,12 @@
 """
 You are a product manager and currently leading a team to develop a new product.
 Unfortunately, the latest version of your product fails the quality check.
-Since each version is developed based on the previous version, all the versions after a bad version are also bad.
 
-Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+Since each version is developed based on the previous version,
+all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one,
+which causes all the following ones to be bad.
 
 You are given an API bool isBadVersion(version) which will return whether version is bad.
 Implement a function to find the first bad version.
@@ -31,12 +34,10 @@ class Solution:
         :rtype: int
         """
         l, r = 1, n
-        while l<=r:
+        while l<r:
             m = l+(r-l)//2
             if isBadVersion(m):
-                if m-1<l or (not isBadVersion(m-1)):
-                    return m
-                r = m-1
+                r = m
             else:
                 l = m+1
-        return 0
+        return l
