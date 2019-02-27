01aba46f8ff2559fa32a12805d4e44941827be99
ellinx <ellin.xll@gmail.com>
2019-02-26 21:09:42 -0800

    Update Serialize and Deserialize BST.

--- SerializeAndDeserializeBST.py
+++ SerializeAndDeserializeBST.py
@@ -1,7 +1,7 @@
 """
-Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored
-in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or
-another computer environment.
+Serialization is the process of converting a data structure or object into a sequence of bits
+so that it can be stored in a file or memory buffer, or transmitted across a network connection
+link to be reconstructed later in the same or another computer environment.
 
 Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how your
 serialization/deserialization algorithm should work. You just need to ensure that a binary search tree can be
@@ -9,8 +9,8 @@ serialized to a string and this string can be deserialized to the original tree 
 
 The encoded string should be as compact as possible.
 
-Note: Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms should
-be stateless.
+Note: Do not use class member/global/static variables to store states.
+        Your serialize and deserialize algorithms should be stateless.
 """
 
 
@@ -23,24 +23,25 @@ be stateless.
 
 from TreeNode import *
 
+class Codec:
 
-class SerializeAndDeserializeBST:
     def serialize(self, root):
         """Encodes a tree to a single string.
 
         :type root: TreeNode
         :rtype: str
         """
-        if not root:
-            return ""
-        ret = str(root.val)
-        left = self.serialize(root.left)
-        if left!="":
-            ret += ","+left
-        right = self.serialize(root.right)
-        if right!="":
-            ret += ","+right
-        return ret
+        ret = []
+        stk = collections.deque()
+        while root is not None or len(stk)>0:
+            if root is not None:
+                ret.append(str(root.val))
+                stk.append(root)
+                root = root.left
+            else:
+                root = stk.pop()
+                root = root.right
+        return " ".join(ret)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -48,23 +49,28 @@ class SerializeAndDeserializeBST:
         :type data: str
         :rtype: TreeNode
         """
-        #print(data)
-        if data=="":
-            return None
-        vals = data.split(",")
-        ret = TreeNode(int(vals[0]))
-        index = 1
-        while index<len(vals):
-            if int(vals[index])<int(vals[0]):
-                index += 1
+        def insert(root, val):
+            pre = None
+            while root is not None:
+                pre = root
+                if root.val<val:
+                    root = root.right
+                else:
+                    root = root.left
+            if pre.val>val:
+                pre.left = TreeNode(val)
             else:
-                break
-        if index>1:
-            ret.left = self.deserialize(",".join(vals[1:index]))
-        if index<len(vals):
-            ret.right = self.deserialize(",".join(vals[index:]))
+                pre.right = TreeNode(val)
+
+        if len(data)==0:
+            return None
+        data = data.split(" ")
+        ret = TreeNode(int(data[0]))
+        for i in range(1,len(data)):
+            insert(ret, int(data[i]))
         return ret
 
+
 # Your Codec object will be instantiated and called as such:
-# codec = SerializeAndDeserializeBST()
+# codec = Codec()
 # codec.deserialize(codec.serialize(root))
