2a8dff0aa6c79704af77d4158bada8e41f6824c7
ellinx <ellin.xll@gmail.com>
2019-02-27 21:09:39 -0800

    Update Reverse Words in a String.

--- ReverseWordsInAString.py
+++ ReverseWordsInAString.py
@@ -5,34 +5,25 @@ Example:
 
 Input: "the sky is blue",
 Output: "blue is sky the".
-Note:
 
-A word is defined as a sequence of non-space characters.
-Input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces.
-You need to reduce multiple spaces between two words to a single space in the reversed string.
+Note:
+1. A word is defined as a sequence of non-space characters.
+2. Input string may contain leading or trailing spaces.
+    However, your reversed string should not contain leading or trailing spaces.
+3. You need to reduce multiple spaces between two words to a single space in the reversed string.
 """
-class Solution(object):
-    def reverseWords(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        words = []
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        ret = []
         l, r = 0, 0
-        while r<len(s):
-            while l<len(s):
-                if s[l]==' ':
-                    l += 1
-                    continue
-                break
+        while l<len(s):
+            while l<len(s) and s[l]==" ":
+                l += 1
             if l==len(s):
                 break
             r = l
-            while r<len(s):
-                if s[r]!=' ':
-                    r += 1
-                    continue
-                break
-            words.append(s[l:r])
+            while r<len(s) and s[r]!=" ":
+                r += 1
+            ret.append(s[l:r])
             l = r+1
-        return ' '.join(words[::-1])
+        return " ".join(ret[::-1])
