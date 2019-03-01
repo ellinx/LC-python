a34496ef1b21d4677c3a50ccf3785c4424e23328
ellinx <ellin.xll@gmail.com>
2019-02-28 19:54:37 -0800

    Update Binary Tree Level Order Traversal II.

--- BinaryTreeLevelOrderTraversalII.py
+++ BinaryTreeLevelOrderTraversalII.py
@@ -1,5 +1,6 @@
 """
-Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+Given a binary tree, return the bottom-up level order traversal of its nodes' values.
+(ie, from left to right, level by level from leaf to root).
 
 For example:
 Given binary tree [3,9,20,null,null,15,7],
@@ -19,25 +20,27 @@ return its bottom-up level order traversal as:
 ]
 
 """
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
-    def levelOrderBottom(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
         ret = []
-        if not root:
+        if root is None:
             return ret
         cur = [root]
         while len(cur):
-            curVal = []
-            nxt = []
+            vals, nxt = [], []
             for each in cur:
-                curVal.append(each.val)
-                if each.left:
+                vals.append(each.val)
+                if each.left is not None:
                     nxt.append(each.left)
-                if each.right:
+                if each.right is not None:
                     nxt.append(each.right)
-            ret.append(curVal)
+            ret.append(vals)
             cur = nxt
         return ret[::-1]
