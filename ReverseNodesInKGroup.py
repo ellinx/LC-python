3b55b7bb4a8456e6e2faf9dc60e7bff90f37d130
ellinx <ellin.xll@gmail.com>
2019-02-28 19:58:02 -0800

    Update Reverse Nodes in k-Group.

--- ReverseNodesInKGroup.py
+++ ReverseNodesInKGroup.py
@@ -6,50 +6,40 @@ k is a positive integer and is less than or equal to the length of the linked li
 If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
 
 Example:
-
 Given this linked list: 1->2->3->4->5
-
 For k = 2, you should return: 2->1->4->3->5
-
 For k = 3, you should return: 3->2->1->4->5
 
 Note:
-
-Only constant extra memory is allowed.
-You may not alter the values in the list's nodes, only nodes itself may be changed.
+1. Only constant extra memory is allowed.
+2. You may not alter the values in the list's nodes, only nodes itself may be changed.
 """
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution:
-    def reverseKGroup(self, head, k):
-        """
-        :type head: ListNode
-        :type k: int
-        :rtype: ListNode
-        """
-        # ---pos1 pos2---pre cur----
-        if k==1 or not head:
-            return head
-        count = 0
-        cur = head
-        while cur:
-            count += 1
-            cur = cur.next
-        count //= k
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
         dummy = ListNode(0)
         dummy.next = head
-        pos1 = dummy
-        pos2 = head
-        pre = head
-        cur = head.next
-        while count:
-            for _ in range(k-1):
-                nextCur = cur.next
+        cur = dummy
+        total = 0
+        while cur.next is not None:
+            cur = cur.next
+            total += 1
+        total //= k
+        #  --p1 p2-----pre cur---
+        p1 = dummy
+        for _ in range(total):
+            p2 = p1.next
+            pre, cur, cnt = p1, p2, 1
+            while cnt<=k:
+                nxt = cur.next
                 cur.next = pre
-                pre, cur = cur, nextCur
-            pos1.next = pre
-            pos2.next = cur
-            count -= 1
-            if not count:
-                break
-            pos1, pos2 = pos2, cur
-            pre, cur = cur, cur.next
+                pre, cur = cur, nxt
+                cnt += 1
+            p1.next, p2.next = pre, cur
+            p1 = p2
         return dummy.next
