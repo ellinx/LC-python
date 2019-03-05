830678191b55115d873580bbbbe11375e2342f42
ellinx <ellin.xll@gmail.com>
2019-03-04 22:16:12 -0800

    Update Regular Expression Matching.

--- RegularExpressionMatching.py
+++ RegularExpressionMatching.py
@@ -3,14 +3,14 @@ Given an input string (s) and a pattern (p), implement regular expression matchi
 
 '.' Matches any single character.
 '*' Matches zero or more of the preceding element.
+
 The matching should cover the entire input string (not partial).
 
 Note:
-s could be empty and contains only lowercase letters a-z.
-p could be empty and contains only lowercase letters a-z, and characters like . or *.
+1. s could be empty and contains only lowercase letters a-z.
+2. p could be empty and contains only lowercase letters a-z, and characters like . or *.
 
 Example 1:
-
 Input:
 s = "aa"
 p = "a"
@@ -18,7 +18,6 @@ Output: false
 Explanation: "a" does not match the entire string "aa".
 
 Example 2:
-
 Input:
 s = "aa"
 p = "a*"
@@ -26,7 +25,6 @@ Output: true
 Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
 
 Example 3:
-
 Input:
 s = "ab"
 p = ".*"
@@ -34,7 +32,6 @@ Output: true
 Explanation: ".*" means "zero or more (*) of any character (.)".
 
 Example 4:
-
 Input:
 s = "aab"
 p = "c*a*b"
@@ -42,41 +39,28 @@ Output: true
 Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore it matches "aab".
 
 Example 5:
-
 Input:
 s = "mississippi"
 p = "mis*is*p*."
 Output: false
 """
 class Solution:
-    def isMatch(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: bool
-        """
+    def isMatch(self, s: str, p: str) -> bool:
         m, n = len(s), len(p)
-        # dp[i][j] stands for if s[:i] matches p[:j]
         dp = [[False]*(n+1) for _ in range(m+1)]
         dp[0][0] = True
-        #first row which is whether p[0:j] match empty string
         for i in range(1,n+1):
-            if p[i-1] != "*":
-                continue
-            dp[0][i] = dp[0][i-2]
+            if p[i-1]=="*":
+                dp[0][i] = dp[0][i-2]
         for i in range(1,m+1):
             for j in range(1,n+1):
-                if p[j-1]=='.' or s[i-1]==p[j-1]:
-                    dp[i][j] = dp[i][j] or dp[i-1][j-1]
-                elif p[j-1]=='*':
-                    # zero char
-                    dp[i][j] = dp[i][j] or dp[i][j-2]
-                    # one or more char
-                    for k in range(i-1,-1,-1):
-                        #print(s,p,i,j)
-                        if dp[i][j] or (p[j-2]!='.' and (s[k]!=s[i-1] or p[j-2]!=s[i-1])):
-                            print(i,j,k)
-                            break
-                        dp[i][j] = dp[i][j] or dp[k][j-2]
-        #print(dp)
+                if s[i-1]==p[j-1] or p[j-1]==".":
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1]=="*":
+                    if s[i-1]==p[j-2] or p[j-2]==".":
+                        # * match zero, one or more characters
+                        dp[i][j] = dp[i][j-2] or dp[i-1][j-2] or dp[i-1][j]
+                    else:
+                        # * only match zero character
+                        dp[i][j] = dp[i][j-2]
         return dp[m][n]
