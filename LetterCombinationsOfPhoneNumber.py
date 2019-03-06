0bc08818b5a855ab7680c8ada20a4469190580aa
ellinx <ellin.xll@gmail.com>
2019-03-05 20:19:34 -0800

    Update Letter Combinations of a Phone Number.

--- LetterCombinationsOfPhoneNumber.py
+++ LetterCombinationsOfPhoneNumber.py
@@ -1,33 +1,27 @@
 """
 Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
 
-A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+A mapping of digit to letters (just like on the telephone buttons) is given below.
+Note that 1 does not map to any letters.
 
 Example:
-
 Input: "23"
 Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 """
-
 class Solution:
-    # use dfs
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
-        def dfs(digits, start, cur, ret):
-            if start == len(digits):
-                ret.append(cur)
+    # DFS
+    def letterCombinations(self, digits: str) -> List[str]:
+        def dfs(mp, digits, start, cur, ret):
+            if start==len(digits):
+                if len(cur)>0:
+                    ret.append("".join(cur))
                 return
-            mm = ["","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
-            for c in mm[int(digits[start])]:
-                dfs(digits, start+1, cur+c, ret)
+            for c in mp[int(digits[start])]:
+                dfs(mp, digits, start+1, cur+[c], ret)
 
+        mp = ["","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
         ret = []
-        if len(digits) == 0:
-            return []
-        dfs(digits, 0, "", ret)
+        dfs(mp, digits, 0, [], ret)
         return ret
 
 class Solution2:
