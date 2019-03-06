746c83137bea29b52b9ddf5d0b62016506b082df
ellinx <ellin.xll@gmail.com>
2019-03-05 20:24:54 -0800

    Update Range Sum Query 2D - Immutable.

--- RangeSumQuery2D-Immutable.py
+++ RangeSumQuery2D-Immutable.py
@@ -25,32 +25,23 @@ You may assume that row1 ≤ row2 and col1 ≤ col2.
 """
 class NumMatrix:
 
-    def __init__(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        """
+    def __init__(self, matrix: List[List[int]]):
         m = len(matrix)
         if m==0:
             return
         n = len(matrix[0])
         self.sum = [[0]*n for _ in range(m)]
-        self.sum[0][0] = matrix[0][0]
-        for i in range(1,n):
-            self.sum[0][i] = self.sum[0][i-1]+matrix[0][i]
+        total = 0
+        for i in range(n):
+            total += matrix[0][i]
+            self.sum[0][i] = total
         for i in range(1,m):
             self.sum[i][0] = self.sum[i-1][0]+matrix[i][0]
         for i in range(1,m):
             for j in range(1,n):
-                self.sum[i][j] = self.sum[i-1][j]+self.sum[i][j-1]-self.sum[i-1][j-1]+matrix[i][j]
+                self.sum[i][j] = matrix[i][j]+self.sum[i-1][j]+self.sum[i][j-1]-self.sum[i-1][j-1]
 
-    def sumRegion(self, row1, col1, row2, col2):
-        """
-        :type row1: int
-        :type col1: int
-        :type row2: int
-        :type col2: int
-        :rtype: int
-        """
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         if row1==0 and col1==0:
             return self.sum[row2][col2]
         if row1==0:
