d579c4ecab90ade98e0def6aa2c8c478ef6c2066
ellinx <ellin.xll@gmail.com>
2019-03-05 20:20:16 -0800

    Update Meeting Rooms.

--- MeetingRooms.py
+++ MeetingRooms.py
@@ -18,13 +18,9 @@ Output: tru
 #         self.end = e
 
 class Solution:
-    def canAttendMeetings(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: bool
-        """
-        intervals.sort(key=lambda i:(i.start,i.end))
-        for i in range(1,len(intervals)):
-            if intervals[i-1].end>intervals[i].start:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x:[x.start,x.end])
+        for i in range(len(intervals)-1):
+            if intervals[i].end>intervals[i+1].start:
                 return False
         return True
