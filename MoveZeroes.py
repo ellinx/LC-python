e0f8c6aaffd673c26ed7660777a4db8d240050e9
ellinx <ellin.xll@gmail.com>
2019-03-05 20:22:21 -0800

    Update Move Zeroes.

--- MoveZeroes.py
+++ MoveZeroes.py
@@ -11,17 +11,15 @@ You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
 """
 class Solution:
-    def moveZeroes(self, nums):
+    def moveZeroes(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: void Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-        i1, i2 = 0, 0
-        while i2<len(nums):
-            if nums[i2]!=0:
-                nums[i1] = nums[i2]
-                i1 += 1
-            i2 += 1
-        while i1<len(nums):
-            nums[i1] = 0
-            i1 += 1
+        idx = 0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[idx]=nums[i]
+                idx += 1
+        while idx<len(nums):
+            nums[idx] = 0
+            idx += 1
