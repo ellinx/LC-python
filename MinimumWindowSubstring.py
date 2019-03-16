62cdc4bc296a42bccc1da50c22638e912f250f79
ellinx <ellin.xll@gmail.com>
2019-03-15 20:45:14 -0700

    Update Minimum Window Substring.

--- MinimumWindowSubstring.py
+++ MinimumWindowSubstring.py
@@ -7,35 +7,33 @@ Input: S = "ADOBECODEBANC", T = "ABC"
 Output: "BANC"
 
 Note:
-
-    If there is no such window in S that covers all characters in T, return the empty string "".
-    If there is such window, you are guaranteed that there will always be only one unique minimum window in S.
-
+1. If there is no such window in S that covers all characters in T, return the empty string "".
+2. If there is such window, you are guaranteed that there will always be only one unique minimum window in S.
 
 """
 class Solution:
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
+    def minWindow(self, s: str, t: str) -> str:
         counter = collections.Counter(t)
         total = len(t)
         ret = ""
-        start, end = 0, 0
-        while end<len(s):
-            if s[end] in counter:
-                if counter[s[end]]>0:
-                    total -= 1
-                counter[s[end]] -= 1
-            end += 1
+        l, r = 0, 0
+        while r<len(s):
+            # print(l,r)
+            if s[r] not in counter:
+                r += 1
+                continue
+            if counter[s[r]]>0:
+                total -= 1
+            counter[s[r]] -= 1
+            r += 1
             while total==0:
-                if len(ret)==0 or len(ret)>end-start:
-                    ret = s[start:end]
-                if s[start] in counter:
-                    if counter[s[start]]>=0:
-                        total += 1
-                    counter[s[start]] += 1
-                start += 1
+                if ret=="" or len(ret)>r-l:
+                    ret = s[l:r]
+                if s[l] not in counter:
+                    l += 1
+                    continue
+                if counter[s[l]]>=0:
+                    total += 1
+                counter[s[l]] += 1
+                l += 1
         return ret
