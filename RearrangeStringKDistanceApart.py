4c2054902937191070be6f7a33b6df307beba98c
ellinx <ellin.xll@gmail.com>
2019-04-02 21:02:14 -0700

    Update Rearrange String k Distance Apart.

--- RearrangeStringKDistanceApart.py
+++ RearrangeStringKDistanceApart.py
@@ -1,30 +1,47 @@
 import heapq, collections
 """
+Given a non-empty string s and an integer k,
+rearrange the string such that the same characters are at least distance k from each other.
 
+All input strings are given in lowercase letters.
+If it is not possible to rearrange the string, return an empty string "".
+
+Example 1:
+Input: s = "aabbcc", k = 3
+Output: "abcabc"
+Explanation: The same letters are at least distance 3 from each other.
+
+Example 2:
+Input: s = "aaabc", k = 3
+Output: ""
+Explanation: It is not possible to rearrange the string.
+
+Example 3:
+Input: s = "aaadbbcc", k = 2
+Output: "abacabcd"
+Explanation: The same letters are at least distance 2 from each other.
 """
 class Solution:
-    def rearrangeString(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: str
-        """
+    def rearrangeString(self, s: str, k: int) -> str:
         if k<=1:
             return s
-        q = collections.deque()
         counter = collections.Counter(s)
-        maxHeap = [(-counter[k], k) for k in counter]
-        heapq.heapify(maxHeap)
-        ret = ""
-        while len(maxHeap):
-            cur = heapq.heappop(maxHeap)
-            ret += cur[1]
-            q.append((cur[0]+1,cur[1]))
-            if len(q)==k:
-                temp = q.popleft()
-                if temp[0]!=0:
-                    heapq.heappush(maxHeap, temp)
-        for each in q:
-            if each[0]!=0:
-                return ""
-        return ret
+        q = collections.deque([None]*k)
+        pq = [[-counter[k],k] for k in counter]
+        heapq.heapify(pq)
+        ret = []
+        while len(pq)>0 or q[0] is not None:
+            cur = q.popleft()
+            if cur is not None:
+                heapq.heappush(pq, cur)
+            num, c = heapq.heappop(pq)
+            ret.append(c)
+            num += 1
+            if num<0:
+                q.append([num,c])
+            else:
+                q.append(None)
+            #print(ret,q)
+        if len(ret)==len(s):
+            return "".join(ret)
+        return ""
