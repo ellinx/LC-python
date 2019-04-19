09223ff1602172eb3c408f9761500c056ee4baa7
ellinx <ellin.xll@gmail.com>
2019-04-18 20:53:12 -0700

    Update Single Number.

--- SingleNumber.py
+++ SingleNumber.py
@@ -1,9 +1,10 @@
 """
-Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+Given a non-empty array of integers, every element appears twice except for one.
+Find that single one.
 
 Note:
-
-Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+Your algorithm should have a linear runtime complexity.
+Could you implement it without using extra memory?
 
 Example 1:
 Input: [2,2,1]
@@ -16,7 +17,7 @@ Output: 4
 """
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ret = 0
-        for each in nums:
-            ret ^= each
+        ret = nums[0]
+        for i in range(1,len(nums)):
+            ret ^= nums[i]
         return ret
