602e505fa767bc06040f97f08c74f79e5aa7e75a
ellinx <ellin.xll@gmail.com>
2019-06-27 22:28:06 -0700

    Update House Robber II.

--- HouseRobberII.py
+++ HouseRobberII.py
@@ -3,21 +3,19 @@ You are a professional robber planning to rob houses along a street.
 Each house has a certain amount of money stashed.
 All houses at this place are arranged in a circle.
 That means the first house is the neighbor of the last one.
-Meanwhile, adjacent houses have security system connected and it will automatically contact the police 
+Meanwhile, adjacent houses have security system connected and it will automatically contact the police
 if two adjacent houses were broken into on the same night.
 
 Given a list of non-negative integers representing the amount of money of each house,
 determine the maximum amount of money you can rob tonight without alerting the police.
 
 Example 1:
-
 Input: [2,3,2]
 Output: 3
 Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
              because they are adjacent houses.
 
 Example 2:
-
 Input: [1,2,3,1]
 Output: 4
 Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
@@ -26,28 +24,15 @@ Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
 
 """
 class Solution:
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if len(nums)==0:
-            return 0
+    def rob(self, nums: List[int]) -> int:
+        def robrange(nums, l, r):
+            if l>r:
+                return 0
+            rob0, rob1 = 0, nums[l]
+            for i in range(l+1, r+1):
+                rob0, rob1 = max(rob0, rob1), rob0+nums[i]
+            return max(rob0, rob1)
+
         if len(nums)==1:
             return nums[0]
-        if len(nums)<=3:
-            return max(nums)
-        ret = 0
-        # rob 0
-        rob0 = nums[0]
-        rob1 = nums[0]+nums[2]
-        for i in range(3,len(nums)-1):
-            rob0, rob1 = max(rob0,rob1), rob0+nums[i]
-        ret = max(ret,rob0,rob1)
-        # not rob 0
-        rob0 = 0
-        rob1 = nums[1]
-        for i in range(2, len(nums)):
-            rob0, rob1 = max(rob0,rob1), rob0+nums[i]
-        ret = max(ret, rob0, rob1)
-        return ret
+        return max(robrange(nums,0,len(nums)-2), robrange(nums,1,len(nums)-1))
