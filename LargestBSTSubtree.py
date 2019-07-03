73df2a20177efbbcef6eaa9151da511e9e43e788
ellinx <ellin.xll@gmail.com>
2019-07-02 22:59:43 -0700

    Update Largest BST Subtree.

--- LargestBSTSubtree.py
+++ LargestBSTSubtree.py
@@ -1,5 +1,6 @@
 """
-Given a binary tree, find the largest subtree which is a Binary Search Tree (BST), where largest means subtree with largest number of nodes in it.
+Given a binary tree, find the largest subtree which is a Binary Search Tree (BST),
+where largest means subtree with largest number of nodes in it.
 
 Note:
 A subtree must include all of its descendants.
@@ -23,50 +24,38 @@ Follow up:
 Can you figure out ways to solve it with O(n) time complexity?
 """
 class Solution:
-    """
-    Thoughts:
-    1. recursive function that takes a tree node and return [min, max, num] where min is minimum val of this subtree, max is maximum value of this subtree and num  is total number of nodes in this subtree
-
-    Time: O(n) where n is number of nodes in the tree
-    Space: O(1)
-    """
-    def largestBSTSubtree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        def helper(root):
-            if not root:
+    def largestBSTSubtree(self, root: TreeNode) -> int:
+        def getInfo(root):
+            nonlocal ret
+            if root is None:
                 return None
-            left = helper(root.left)
-            right = helper(root.right)
-            ret = [0]*3
-            if left:
-                if left[2]==0:
-                    return [0]*3
-                if root.val>left[1]:
-                    ret[0] = left[0]
-                    ret[2] += left[2]
-                else:
-                    ret[2] = -1
-            else:
-                ret[0] = root.val
-            if right:
-                if right[2]==0:
-                    return [0]*3
-                if root.val<right[0] and right[2]:
-                    ret[1] = right[1]
-                    ret[2] += right[2]
-                else:
-                    ret[2] = -1
-            else:
-                ret[1] = root.val
-            ret[2] += 1
-            nonlocal maxBST
-            maxBST = max(maxBST, ret[2])
-            #print(ret,root.val)
-            return ret
+            if root.left is None and root.right is None:
+                ret = max(ret, 1)
+                #print(root.val, ret)
+                return [root.val, root.val, root.val, 1]
+            if root.left is None:
+                r = getInfo(root.right)
+                if r is not None and root.val < r[0]:
+                    ret = max(ret, r[3]+1)
+                    #print(root.val, ret)
+                    return [root.val, root.val, r[2], r[3]+1]
+                return None
+            if root.right is None:
+                l = getInfo(root.left)
+                if l is not None and root.val > l[2]:
+                    ret = max(ret, l[3]+1)
+                    #print(root.val, ret)
+                    return [l[0], root.val, root.val, l[3]+1]
+                return None
+            l, r = getInfo(root.left), getInfo(root.right)
+            if l is None or r is None:
+                return None
+            if l[2]<root.val and root.val<r[0]:
+                ret = max(ret, l[3]+r[3]+1)
+                #print(root.val, ret)
+                return [l[0], root.val, r[2], l[3]+r[3]+1]
+            return None
 
-        maxBST = 0
-        helper(root)
-        return maxBST
+        ret = 0
+        getInfo(root)
+        return ret
