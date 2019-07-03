eb844bf5a5195c27effd1bd6165c96fd6fef8abb
ellinx <ellin.xll@gmail.com>
2019-07-02 23:02:17 -0700

    Update Remove Linked List Elements.

--- RemoveLinkedListElements.py
+++ RemoveLinkedListElements.py
@@ -6,25 +6,19 @@ Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
 Return: 1 --> 2 --> 3 --> 4 --> 5
 """
 class Solution:
-    def removeElements(self, head, val):
-        """
-        :type head: ListNode
-        :type val: int
-        :rtype: ListNode
-        """
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
         dummy = ListNode(0)
         dummy.next = head
-        cur = dummy
-        while cur.next is not None:
-            if cur.next.val == val:
-                cur.next = cur.next.next
+        pre = dummy
+        while pre.next is not None:
+            if pre.next.val == val:
+                pre.next = pre.next.next
             else:
-                cur = cur.next
+                pre = pre.next
         return dummy.next
 
 
 # test
-from ListNode import *
 if __name__=="__main__":
     test = Solution()
     head = initList([1,2,6,3,4,5,6])
