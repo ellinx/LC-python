01653e569c112cd6136fc2c3a32345619839846f
ellinx <ellin.xll@gmail.com>
2020-08-16 12:37:51 -0700

    Update Non-overlapping Intervals.

--- NonoverlappingIntervals.py
+++ NonoverlappingIntervals.py
@@ -20,37 +20,33 @@ Example 3:
 Input: [ [1,2], [2,3] ]
 Output: 0
 Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
-"""
-from Interval import *
-# Definition for an interval.
-# class Interval:
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
 
-class Solution:
-    def eraseOverlapIntervals(self, intervals: 'List[Interval]') -> 'int':
-        if len(intervals)<=1:
+
+Note:
+   1. You may assume the interval's end point is always bigger than its start point.
+   2. Intervals like [1,2] and [2,3] have borders "touching" but they don't overlap each other.
+
+"""
+from typing import List
+
+class NonoverlappingIntervals:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        if len(intervals) == 0:
             return 0
-        intervals.sort(key=lambda x:[x.end, -x.start])
-        cur = intervals[0]
         ret = 0
-        for i in range(1, len(intervals)):
-            if intervals[i].start>=cur.end:
+        intervals.sort(key=lambda x:[x[1], -x[0]])
+        cur = intervals[0]
+        for i in range(1,len(intervals)):
+            if cur[1] > intervals[i][0]:
+                ret += 1
+            else:
                 cur = intervals[i]
-                continue
-            ret += 1
         return ret
 
 
 # test
 if __name__=="__main__":
     tmp = NonoverlappingIntervals()
-    intervals = [
-        Interval(1,2),
-        Interval(2,3),
-        Interval(3,4),
-        Interval(1,3)
-    ]
+    intervals = [[1,2],[2,3],[3,4],[1,3]]
     result = tmp.eraseOverlapIntervals(intervals)
     print(result)
