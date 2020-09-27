146814276f7890d6e37f7b80e065d1cb747d10a5
ellinx <ellin.xll@gmail.com>
2020-09-27 10:53:14 -0700

    Update Evaluate Division.

--- EvaluateDivision.py
+++ EvaluateDivision.py
@@ -1,57 +1,77 @@
 """
-Equations are given in the format A / B = k, where A and B are variables represented as strings, and k is a real number (floating point number). Given some queries, return the answers. If the answer does not exist, return -1.0.
+You are given equations in the format A / B = k, where A and B are variables represented as strings, and k is a real number (floating-point number). Given some queries, return the answers. If the answer does not exist, return -1.0.
 
-Example:
-Given a / b = 2.0, b / c = 3.0.
-queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ? .
-return [6.0, 0.5, -1.0, 1.0, -1.0 ].
-
-The input is: vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries , where equations.size() == values.size(), and the values are positive. This represents the equations. Return vector<double>.
-
-According to the example above:
-
-equations = [ ["a", "b"], ["b", "c"] ],
-values = [2.0, 3.0],
-queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ].
 The input is always valid. You may assume that evaluating the queries will result in no division by zero and there is no contradiction.
+
+ 
+
+Example 1:
+
+Input: equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
+Explanation: 
+Given: a / b = 2.0, b / c = 3.0
+queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
+return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
+
+Example 2:
+
+Input: equations = [["a","b"],["b","c"],["bc","cd"]], values = [1.5,2.5,5.0], queries = [["a","c"],["c","b"],["bc","cd"],["cd","bc"]]
+Output: [3.75000,0.40000,5.00000,0.20000]
+
+Example 3:
+
+Input: equations = [["a","b"]], values = [0.5], queries = [["a","b"],["b","a"],["a","c"],["x","y"]]
+Output: [0.50000,2.00000,-1.00000,-1.00000]
+
+ 
+
+Constraints:
+
+    1 <= equations.length <= 20
+    equations[i].length == 2
+    1 <= equations[i][0], equations[i][1] <= 5
+    values.length == equations.length
+    0.0 < values[i] <= 20.0
+    1 <= queries.length <= 20
+    queries[i].length == 2
+    1 <= queries[i][0], queries[i][1] <= 5
+    equations[i][0], equations[i][1], queries[i][0], queries[i][1] consist of lower case English letters and digits.
+
 """
 
-class EvaluateDivision:
-    def calcEquation(self, equations, values, queries):
-        """
-        :type equations: List[List[str]]
-        :type values: List[float]
-        :type queries: List[List[str]]
-        :rtype: List[float]
-        """
+import collections
+from typing import List, Dict, Set
+
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        def dfs(g: Dict[str,Dict[str,float]], a: str, b: str, mm: Set):
+            if a not in g or b not in g:
+                return -1
+            if a == b:
+                return 1
+            mm.add(a)
+            for each in g[a]:
+                if each not in mm:
+                    tmp = dfs(g, each, b, mm)
+                    if tmp != -1:
+                        return g[a][each] * tmp
+            return -1
+        
         g = collections.defaultdict(dict)
         for i in range(len(equations)):
             g[equations[i][0]][equations[i][1]] = values[i]
-            g[equations[i][1]][equations[i][0]] = 1.0/values[i]
+            g[equations[i][1]][equations[i][0]] = 1/values[i]
+        #print(g)
         ret = []
-        for query in queries:
-            if query[0] not in g:
-                ret.append(-1.0)
-                continue
-            if query[0]==query[1]:
-                ret.append(1.0)
-                continue
-            # BFS
-            visited = set()
-            visited.add(query[0])
-            q = collections.deque()
-            for k in g[query[0]]:
-                visited.add(k)
-                q.append((k, g[query[0]][k]))
-            while len(q):
-                cur = q.popleft()
-                if cur[0]==query[1]:
-                    ret.append(cur[1])
-                    break
-                for k in g[cur[0]]:
-                    if k not in visited:
-                        visited.add(k)
-                        q.append((k, cur[1]*g[cur[0]][k]))
-            else:
-                ret.append(-1.0)
+        for a,b in queries:
+            ret.append(dfs(g,a,b,set()))
         return ret
+
+
+if __name__ == "__main__":
+    test = Solution()
+    equations = [["a","b"],["b","c"]]
+    values =[2.0, 3.0]
+    queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+    print(test.calcEquation(equations, values, queries))
